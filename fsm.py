d5ad350b20d03e8990f38167249c7f291b2d11b9
Wei <zxc33991177@gmail.com>
2020-12-16 21:20:55 +0800

    template test

--- fsm.py
+++ fsm.py
@@ -7,30 +7,61 @@ class TocMachine(GraphMachine):
     def __init__(self, **machine_configs):
         self.machine = GraphMachine(model=self, **machine_configs)
 
-    def is_going_to_state1(self, event):
+    def is_going_to_mainmenu(self, event):
         text = event.message.text
-        return text.lower() == "go to state1"
+        return text == "主選單"
 
-    def is_going_to_state2(self, event):
+    def is_going_to_introduction(self, event):
         text = event.message.text
-        return text.lower() == "go to state2"
+        return text == "功能介紹"
 
-    def on_enter_state1(self, event):
-        print("I'm entering state1")
+    def is_going_to_keep_accounts(self, event):
+        text = event.message.text
+        return text == "記帳"
 
+    def is_going_to_watch_chart(self, event):
+        text = event.message.text
+        return text == "查看收支圖表"
+
+    def is_going_to_watch_balance(self, event):
+        text = event.message.text
+        return text == "本月目前結餘"
+
+    def on_enter_mainmenu(self, event):
         reply_token = event.reply_token
-        send_text_message(reply_token, "Trigger state1")
+        message = message_template.main_menu
+        message_to_reply = FlexSendMessage("開啟主選單", message)
+        line_bot_api = LineBotApi( os.getenv('LINE_CHANNEL_ACCESS_TOKEN') )
+        line_bot_api.reply_message(reply_token, message_to_reply)
+
+    def on_enter_introduction(self, event):
+        reply_token = event.reply_token
+        message = message_template.introduction_message
+        message_to_reply = FlexSendMessage("開啟功能介紹", message)
+        line_bot_api = LineBotApi( os.getenv('LINE_CHANNEL_ACCESS_TOKEN') )
+        line_bot_api.reply_message(reply_token, message_to_reply)
         self.go_back()
 
-    def on_exit_state1(self):
-        print("Leaving state1")
-
-    def on_enter_state2(self, event):
-        print("I'm entering state2")
-
+    def on_enter_keep_accounts(self, event):
         reply_token = event.reply_token
-        send_text_message(reply_token, "Trigger state2")
+        message = message_template.introduction_message
+        message_to_reply = FlexSendMessage("開啟記帳功能", message)
+        line_bot_api = LineBotApi( os.getenv('LINE_CHANNEL_ACCESS_TOKEN') )
+        line_bot_api.reply_message(reply_token, message_to_reply)
         self.go_back()
 
-    def on_exit_state2(self):
-        print("Leaving state2")
+    def on_enter_watch_chart(self, event):
+        reply_token = event.reply_token
+        message = message_template.introduction_message
+        message_to_reply = FlexSendMessage("開啟圖表", message)
+        line_bot_api = LineBotApi( os.getenv('LINE_CHANNEL_ACCESS_TOKEN') )
+        line_bot_api.reply_message(reply_token, message_to_reply)
+        self.go_back()
+
+    def on_enter_watch_balance(self, event):
+        reply_token = event.reply_token
+        message = message_template.introduction_message
+        message_to_reply = FlexSendMessage("開啟結餘", message)
+        line_bot_api = LineBotApi( os.getenv('LINE_CHANNEL_ACCESS_TOKEN') )
+        line_bot_api.reply_message(reply_token, message_to_reply)
+        self.go_back()
---
+++ template.py
@@ -0,0 +1,59 @@
+{
+  "type": "carousel",
+  "contents": [
+    {
+      "type": "bubble",
+      "size": "mega",
+      "hero": {
+        "type": "image",
+        "url": "https://i.imgur.com/KhzZ5X2.png"
+      },
+      "footer": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "button",
+            "action": {
+              "type": "message",
+              "label": "介紹",
+              "text": "功能介紹"
+            },
+            "color": "#ffffff",
+            "offsetBottom": "sm"
+          },
+          {
+            "type": "spacer",
+            "size": "xs"
+          }
+        ],
+        "backgroundColor": "#ff9900",
+        "offsetTop": "lg"
+      }
+    },
+    {
+      "type": "bubble",
+      "body": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": []
+      }
+    },
+    {
+      "type": "bubble",
+      "body": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": []
+      }
+    },
+    {
+      "type": "bubble",
+      "body": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": []
+      }
+    }
+  ]
+}
